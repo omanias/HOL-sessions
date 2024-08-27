aa90a27323ffb85e06c587546f51ee2b9c07c6b9
omarmaniaslh <omar@legalhub.la>
2024-08-26 22:05:04 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import { engine } from 'express-handlebars';
+import mongoose from './config/database.js';
+import MongoStore from 'connect-mongo';
+import sessionsRouter from './routes/api/sessions.js';
+import viewsRouter from './routes/views.js';
+
+const app = express();
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'hbs');
+app.set('views', '../src/views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: '' }),
+    // cookie: { maxAge: 180 * 60 * 1000 },
+}));
+
+app.use('/api/sessions', sessionsRouter);
+app.use('/', viewsRouter);
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
